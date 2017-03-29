551161b2ca8f5dbd291ef52b0f9a18e1785e17e9
Eelis Mikkola <eelis.mikkola@student.tut.fi>
2017-03-29 18:35:27 +0300

    Created a basic testscene with a crappy physics based character controller (moving & jumping) and camerafollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private bool lockY = true;
+    [SerializeField]
+    private GameObject player;
+
+    private Transform mainCamTransform;
+    private Transform playerTransform;
+	private Vector3 prevCamPos;
+
+	// Use this for initialization
+	void Start ()
+	{
+		findPlayer();
+        cacheTransforms();
+        prevCamPos = new Vector3 (mainCamTransform.position.x, mainCamTransform.position.y, mainCamTransform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if ( player.activeSelf )
+		{
+            Vector3 camerapos = mainCamTransform.position;
+            Vector3 playerpos = playerTransform.position;
+			Vector3 cameraChange = Vector3.Lerp(camerapos, playerpos, 1f*Time.deltaTime);
+            if (lockY)
+            {
+                cameraChange.y = prevCamPos.y;
+            }
+            cameraChange.z = prevCamPos.z;
+            mainCamTransform.position = cameraChange;
+			prevCamPos.Set(cameraChange.x, cameraChange.y, cameraChange.z);
+		}
+
+	}
+
+	// Tries to find the player. Call in Start()
+	private void findPlayer()
+	{
+		if (player == null)
+		{
+            GameObject pl = GameObject.FindGameObjectWithTag("Player");
+			if (pl == null)
+			{
+				Debug.Log("Can't find 'Player' game object");
+			}
+			else
+			{
+				player = pl;
+			}
+		}
+	}
+
+    private void cacheTransforms()
+    {
+        if (player != null)
+        {
+            playerTransform = player.transform;
+        }
+        mainCamTransform = Camera.main.transform;
+    }
+}
