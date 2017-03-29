a1bb5ae842d1cc5506caf73970f7342d3875f515
jalkanen <jalkanen@git.rd.tut.fi>
2017-03-29 18:47:45 +0300

    spaace

---
+++ Assets/orbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbit : MonoBehaviour {
+
+    [SerializeField]
+    private float rotation = 5;
+    [SerializeField]
+    private float selfRotation = 20;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = new Vector3(transform.position.x+Time.deltaTime, 0, 0);
+        transform.RotateAround(new Vector3(0, 0, 0), new Vector3(0, 0, 1), rotation * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * selfRotation));
+	}
+}
