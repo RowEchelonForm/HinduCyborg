bb7580c724bad39585e28f7185262256cfe21abf
jalkanen <jalkanen@git.rd.tut.fi>
2017-04-16 01:08:05 +0300

    Planet scripts and gui

---
+++ Assets/Scripts/Space/Planet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour {
+
+    public string planetName;
+
+    [TextArea(3, 10)]
+    public string description;
+
+    public string level;
+
+    public bool sun;
+}
--- Assets/Scripts/Space/Ship.cs
+++ Assets/Scripts/Space/Ship.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Ship : MonoBehaviour
@@ -8,15 +9,32 @@ public class Ship : MonoBehaviour
 
     [SerializeField]
     private GameObject sun;
+
     [SerializeField]
-    private GameObject planet1;
+    private double bufferTime = 0.01;
     [SerializeField]
-    private double bufferTime = 0.1;
+    private int bufferParts = 40;
+
     [SerializeField]
-    private int bufferParts = 10;
+    private Text planetName;
+    [SerializeField]
+    private Text planetDesc;
+    [SerializeField]
+    private GameObject planetPanel;
+    [SerializeField]
+    private GameObject landPanel;
+    [SerializeField]
+    private Button yes;
+    [SerializeField]
+    private Button no;
 
     private Rigidbody rb;
     private bool onPlanet;
+    private string level;
+    private bool inGui;
+    private int shownGui;
+
+
     private double[] speedParts;
     private double[] speedTimes;
     private double speedBuffer;
@@ -24,7 +42,23 @@ public class Ship : MonoBehaviour
     private Vector3 lastPosition;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
+        yes.onClick.AddListener(yesClick);
+        no.onClick.AddListener(exitGui);
+    }
+
+    void yesClick()
+    {
+        exitGui();
+        LevelManager.loadLevel(level);
+    }
+
+    void exitGui()
+    {
+        planetPanel.SetActive(false);
+        landPanel.SetActive(false);
+        inGui = false;
     }
 
     // Use this for initialization
@@ -32,6 +66,7 @@ public class Ship : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         onPlanet = false;
+        inGui = false;
         speedParts = new double[bufferParts];
         speedTimes = new double[bufferParts];
         for (int i = 0; i < speedParts.Length; i++)
@@ -42,6 +77,8 @@ public class Ship : MonoBehaviour
         lastPosition = rb.position;
         speedBuffer = 0;
         timeBuffer = 0;
+        planetPanel.SetActive(false);
+        landPanel.SetActive(false);
     }
 
     public float avgSpeed()
@@ -57,7 +94,7 @@ public class Ship : MonoBehaviour
             div += i;
         }
         Debug.Log((avgSpeed / div));
-        return (float)(avgSpeed/div);
+        return (float)(avgSpeed / div);
     }
 
     // Update is called once per frame
@@ -65,55 +102,107 @@ public class Ship : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        //transform.position.Set(transform.position.x, transform.position.y, 0);
-        //rb.velocity.Set(rb.velocity.x, rb.velocity.y, 0);
-        speedBuffer += (rb.position-lastPosition).magnitude;
+
+        speedBuffer += (rb.position - lastPosition).magnitude;
         lastPosition = rb.position;
         timeBuffer += Time.deltaTime;
         if (timeBuffer >= bufferTime)
         {
             for (int i = 1; i < speedParts.Length; i++)
             {
-                speedParts[i-1] = speedParts[i];
-                speedTimes[i-1] = speedTimes[i];
+                speedParts[i - 1] = speedParts[i];
+                speedTimes[i - 1] = speedTimes[i];
             }
-            speedParts[speedParts.Length-1] = speedBuffer;
-            speedTimes[speedParts.Length-1] = timeBuffer;
+            speedParts[speedParts.Length - 1] = speedBuffer;
+            speedTimes[speedParts.Length - 1] = timeBuffer;
             speedBuffer = 0;
             timeBuffer = 0;
             avgSpeed();
         }
-        if  (v != 0)
+
+        if (!inGui)
         {
-            rb.AddRelativeForce(0, v * Time.deltaTime * 90, 0);
-            Destroy(GetComponent<FixedJoint>());
-            onPlanet = false;
+            if (v != 0)
+            {
+                rb.AddRelativeForce(0, v * Time.deltaTime * 90, 0);
+                Destroy(GetComponent<FixedJoint>());
+                onPlanet = false;
+            }
+            if (h != 0)
+            {
+                rb.AddTorque(0, 0, -h * Time.deltaTime);
+            }
         }
-        if (h != 0)
+        else
         {
-            rb.AddTorque(0, 0, -h * Time.deltaTime);
+            if (Input.GetButton("Jump") && shownGui == 1)
+            {
+                planetPanel.SetActive(false);
+                landPanel.SetActive(true);
+                shownGui = 2;
+            }
+            else if (shownGui == 2)
+            {
+                if (h < 0)
+                {
+                    yesClick();
+                }
+                else if (h > 0)
+                {
+                    exitGui();
+                }
+            }
+            else if (Input.GetButton("Jump") && shownGui == 3)
+            {
+                LevelManager.reloadCurrentLevel();
+            }
         }
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.Equals(sun))
+        Planet planet = col.gameObject.GetComponent<Planet>();
+        if (planet != null && !onPlanet)
         {
-            //grats on landing on sun
-            LevelManager.reloadCurrentLevel();
-        }
-        else if (col.gameObject.Equals(planet1) && !onPlanet)
-        {
-            if (avgSpeed() < 0.5) {
-                //landed on planet
-                gameObject.AddComponent<FixedJoint>();
-                gameObject.GetComponent<FixedJoint>().connectedBody = col.rigidbody;
+            if (avgSpeed() < 0.5)
+            {
+                //landed on something
+                if (!planet.sun)
+                {
+                    gameObject.AddComponent<FixedJoint>();
+                    gameObject.GetComponent<FixedJoint>().connectedBody = col.rigidbody;
+                    shownGui = 1;
+                    level = planet.level;
+                }
+                else
+                {
+                    //some nice game over?
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    shownGui = 3;
+                }
                 onPlanet = true;
-                LevelManager.loadLevel("Planet01"); // TODO make an another script for the planet that has the level's name etc.
+                inGui = true;
+                planetName.text = planet.planetName;
+                planetDesc.text = planet.description;
+                planetPanel.SetActive(true);
             }
             else
             {
-                //crashlanding
+                if (!planet.sun)
+                {
+                    //going too fast -> take dmg or something
+                }
+                else
+                {
+                    //gg, you made it to afterlife
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                    shownGui = 3;
+                    onPlanet = true;
+                    inGui = true;
+                    planetName.text = planet.planetName;
+                    planetDesc.text = planet.description;
+                    planetPanel.SetActive(true);
+                }
             }
         }
     }
