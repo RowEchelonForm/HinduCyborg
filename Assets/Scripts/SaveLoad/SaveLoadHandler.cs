afd5585bcd060240678ef647518d6dc43f3291fd
jalkanen <jalkanen@git.rd.tut.fi>
2017-05-04 16:35:17 +0300

    save and load wont work yet

---
+++ Assets/Scripts/SaveLoad/SaveLoadHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SaveLoad.Save("2C", transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SaveLoad.Load("2C");
+        }
+	}
+}
