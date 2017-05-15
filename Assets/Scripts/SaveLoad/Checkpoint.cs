e4a2b05f5e830ecd8c22dfe0fe234b439ea686de
Eelis Mikkola <eelis.mikkola@student.tut.fi>
2017-05-15 17:21:43 +0300

    Fixed checkpoint gizmo

--- Assets/Scripts/SaveLoad/Checkpoint.cs
+++ Assets/Scripts/SaveLoad/Checkpoint.cs
@@ -23,9 +23,8 @@ public class Checkpoint : MonoBehaviour
 			hitCheckpoint();
 		}
 	}
-
-
-
+    
+    
 	private void hitCheckpoint()
 	{
 		Debug.Log("Player hit checkpoint '" + gameObject.name + "' with the instanceID " + gameObject.GetInstanceID());
@@ -33,49 +32,32 @@ public class Checkpoint : MonoBehaviour
 		SaveLoad.Save(LevelManager.currentLevelName);
         SaveLoad.SaveToFile(saveFileName);
 	}
-
-
-
+    
+    
+    
+    // Draw the checkpoint gizmo
     protected virtual void OnDrawGizmos()
     {
     	// Variables:
     	float lineHeight = 2f;
     	float triangleLength = lineHeight/4;
-
-		// Starting position:
+        Gizmos.color = Color.red;
+        
+		// Positions for the line:
         Vector3 startingPosition = transform.position;
-
-        // Draw line:
-		Vector3 endPosition = startingPosition + new Vector3(0, lineHeight, 0);
-		Gizmos.color = Color.red;
+        Vector3 endPosition = startingPosition + new Vector3(0, lineHeight, 0); // also used for the triangle
+        
+        // Positions for the triangle
+        Vector3 triangleTip = new Vector3(endPosition.x + triangleLength, endPosition.y-triangleLength, endPosition.z);
+        Vector3 triangleBottom = new Vector3(endPosition.x, endPosition.y-triangleLength, endPosition.z);
+        
+        // Draw the line:
 		Gizmos.DrawLine(startingPosition, endPosition);
-
-		// Draw mesh:
-		Mesh mesh = new Mesh();
-		mesh.vertices = new Vector3[3]
-		{
-			new Vector3(0f, -triangleLength, 0f),
-			new Vector3(triangleLength, 0f, 0f),
-			new Vector3(0f, triangleLength, 0f)
-		};
-		mesh.triangles = new int[]
-		{
-			0, 1, 2
-		};
-		mesh.normals = new Vector3[]
-		{
-			Vector3.forward,
-			Vector3.forward,
-			Vector3.forward
-		};
-		mesh.colors = new Color[]
-		{
-			Color.red,
-			Color.red,
-			Color.red
-		};
-		Graphics.DrawMeshNow(mesh, endPosition, Quaternion.identity);
-		DestroyImmediate(mesh);
+        
+        // Draw the triangle
+        Gizmos.DrawLine(endPosition, triangleTip);
+        Gizmos.DrawLine(triangleTip, triangleBottom);
     }
+
 
 }
