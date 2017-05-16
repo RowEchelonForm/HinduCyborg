e08f8ba0bc73c64febe018dc2e6e41f09936245c
Ali mo <ali.moro.92@gmail.com>
2017-05-16 19:08:29 +0300

    Wall + collapsing animation

---
+++ Assets/Scripts/Wall/Wall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        findComponents();
+        //anim.Play("collapse_animation");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // TODO: do we need these?
+    private void findComponents()
+    {
+        anim = GetComponent<Animator>();
+        if (anim == null)
+        {
+            Debug.LogError("Error: No Animator found on the player from " + this.GetType().ToString() + " script! Please attach it.");
+        }
+
+    }
+}
