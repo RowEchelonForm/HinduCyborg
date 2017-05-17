d6309b5b954b6a243b8ced62a39a6a87da808df0
Eelis Mikkola <eelis.mikkola@student.tut.fi>
2017-05-17 21:39:34 +0300

    Fixed a bug where player's max speed was slowed down permanenetly when interrupting 'jump_finished' animation (dashing/shielding/punching when jump_finished animation was playing).

---
+++ Assets/Scripts/StateMachineBehaviors/JumpFinishedScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * For jump_finished animation.
+ * Calls CharacterMovement script to slow down on landing and then reset speed afterwards.
+*/
+public class JumpFinishedScript : StateMachineBehaviour
+{
+    
+    private CharacterMovement charMov;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+	    if (charMov == null)
+        {
+            charMov = animator.gameObject.GetComponent<CharacterMovement>();
+        }
+        if (charMov != null)
+        {
+            charMov.slowOnLanding();
+        }
+	}
+    
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+	    if (charMov != null)
+        {
+            charMov.landingFinished();
+        }
+	}
+}
